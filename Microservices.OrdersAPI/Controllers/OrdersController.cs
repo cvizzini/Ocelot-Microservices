6a6482d28bbd7c7a48665d5477d7ba9f3511e08c
Claudio Vizzini <claudiov@osiristrading.com>
2022-11-15 14:36:19 +0200

    Add project files.

---
+++ Microservices.OrdersAPI/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.OrdersAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    [HttpGet]
+    public List<string> Get()
+    {
+        List<string> orders = new List<string>
+        {
+            "Order 1",
+            "Order 2",
+            "Order 3",
+            "Order 4"
+        };
+        return orders;
+    }
+}
---
+++ Microservices.ProductsAPI/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.ProductsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            List<string> products = new List<string>();
+            products.Add("Samsung products");
+            products.Add("Apple products");
+            products.Add("Google products");
+            products.Add("Microsoft products");
+            return products;
+        }
+    }
+}
